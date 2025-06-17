3329ed8c6f74385ab5d187507da8675550e0c93d
ethan <ethanmahlke@gmail.com>
2025-06-17 06:38:08 -0700

    background now saved in localstorage

--- scripts/background_changer.js
+++ scripts/background_changer.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     const backgrounds = [
-        'media/background-hills-day.gif',
-        'media/background-hills-night.gif'
+        '/media/background-hills-day.gif',
+        '/media/background-hills-night.gif'
     ];
 
-    let currentBackgroundIndex = 0;
+    let currentBackgroundIndex = parseInt(localStorage.getItem('backgroundIndex')) || 0;
+
+    // apply background
+    document.body.style.backgroundImage = `url('${backgrounds[currentBackgroundIndex]}')`;
 
     const changeBackgroundButton = document.getElementById("menu-item-background"); 
     const backgroundButtonText = changeBackgroundButton.querySelector(".menu-full");
     const backgroundOriginalText = backgroundButtonText.textContent;
+
     changeBackgroundButton.addEventListener("click", function(event) {
         event.preventDefault();
 
-        // cycle through backgrounds
+        // cycle
         currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
         document.body.style.backgroundImage = `url('${backgrounds[currentBackgroundIndex]}')`;
 
-        // update button text
+        // save 
+        localStorage.setItem('backgroundIndex', currentBackgroundIndex);
+
+        // update haha minecraft
         backgroundButtonText.textContent = currentBackgroundIndex === 0 ? "/time set day" : "/time set night";
         setTimeout(function() {
             backgroundButtonText.textContent = backgroundOriginalText;
